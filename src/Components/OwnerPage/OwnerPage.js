307e53838969479d5f72b4417d90644fdf6e7620
John Gellert <gellertjm@gmail.com>
2019-07-18 11:27:50 -0500

    Add OwnerPage component.

---
+++ src/Components/OwnerPage/OwnerPage.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+class OwnerPage extends Component() {
+
+    state = {
+        owner_name: '',
+
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            owner_name: event.target.value,
+        });
+    }
+
+    handleSubmitClick = () => {
+        // this.props.dispatch({type: 'POST_PET_OWNER', payload: this.state.owner_name});
+        console.log('click');
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <TextField required defaultValue={this.state.owner_name} onChange={this.handleNameChange}></TextField>
+                <Button onClick={this.handleSubmitClick}>Submit</Button>
+                <pre>
+                    {JSON.stringify(this.state, null, 2)}
+                </pre>
+            </div>
+
+        );
+    }
+}
+
+export default OwnerPage;
