6220b2db8e2fedd569b58f406a6e1bba02112786
John Gellert <gellertjm@gmail.com>
2019-07-19 10:46:13 -0500

    Add button styles and updated functionality.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import OwnerPage from '../OwnerPage/OwnerPage';
+import Button from '@material-ui/core/Button';
 
 function App() {
 
@@ -10,10 +11,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"><h1>Pet Hotel</h1></header>
-      <Button onClick={()=>{setDashboard(!isDashboard)}}>Dashboard</Button>
-      <Button onClick={()=>{setDashboard(!isDashboard)}}>Manage Owner</Button>
+      <Button
+        variant={isDashboard && "contained"}
+        onClick={isDashboard === false && (() => { setDashboard(!isDashboard) })}
+      >
+        Dashboard
+      </Button>
+      <Button
+        variant={isDashboard === false && "contained"}
+        onClick={isDashboard === true && (() => { setDashboard(!isDashboard) })}
+      >
+        Manage Owner
+      </Button>
       {isDashboard && <Dashboard /> || <OwnerPage />}
-      
+
     </div>
   );
 }
