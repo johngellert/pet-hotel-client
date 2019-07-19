9c586c6fc4f0e455591c90e23c155d956c81b5a5
John Gellert <gellertjm@gmail.com>
2019-07-19 10:31:21 -0500

    Add buttons and state to toggle between dashboard and manage owners view.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
 import OwnerPage from '../OwnerPage/OwnerPage';
 
 function App() {
+
+  const [isDashboard, setDashboard] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header"><h1>Pet Hotel</h1></header>
-      <Dashboard />
-      <OwnerPage />
+      <Button onClick={()=>{setDashboard(!isDashboard)}}>Dashboard</Button>
+      <Button onClick={()=>{setDashboard(!isDashboard)}}>Manage Owner</Button>
+      {isDashboard && <Dashboard /> || <OwnerPage />}
+      
     </div>
   );
 }
